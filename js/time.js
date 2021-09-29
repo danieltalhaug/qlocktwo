9f7716075cfe10b88cd467e2d03b84988be9bda7
danieltalhaug <daniel.talhaug@outlook.com>
2021-09-29 15:02:59 +0200

    Fix converting to 12 hour format

--- js/time.js
+++ js/time.js
@@ -2,7 +2,7 @@ export const getTime = () => {
     const now = new Date();
     return {
         // get 12 hour format without having to deal with toLocaleString()
-        hours: now.getHours() <= 13 ? now.getHours() : Math.floor((now.getHours() / 2)),
+        hours: now.getHours() % 12 || 12,
         minutes: now.getMinutes(),
     };
 };
