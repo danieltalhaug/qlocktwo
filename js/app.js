2ebc17d2652d02dab1f94d146794f3ebed029621
danieltalhaug <daniel.talhaug@outlook.com>
2021-09-29 22:24:30 +0200

    Render clock with current time

--- js/app.js
+++ js/app.js
@@ -1,21 +1,45 @@
-import {clockLetters} from './letters.js';
-import {getTime} from './time.js';
+import { clockLetters } from './letters.js';
+import { getTime } from './time.js';
+import { renderClockLetters } from './dom-manipulations.js'
 
-const renderLetters = (letters) => {
-    const clock = document.getElementById("clock");
+const init = () => {
+    const letters = clockLetters();
+    const { hours, minutes } = getTime();
 
     letters.forEach(letter => {
-        const letterElement = document.createElement('div');
-        const letterContent = document.createTextNode(letter.letter);
-
-        // Adds the classes from the class array
-        for(let i = 0; i < letter.class.length; i++) {
-            letterElement.classList.add(letter.class[i]);
+        // Show obligatory letters
+        if (letter.type === 'obligatory') {
+            letter.class.push('active');
+            
+            return;
         }
 
-        letterElement.appendChild(letterContent);
-        clock.appendChild(letterElement);
+        // For hours
+        if (letter.type === 'hour') {
+            if (minutes > 34) {
+                // Check if it's past half an hour, 34 because 30 would for example
+                // show Half past seven when it's halv past six
+                if (letter.rel.includes(hours + 1)) {
+                    letter.class.push('active');
+                }
+            } else {
+                if (letter.rel.includes(hours)) {
+                    letter.class.push('active');
+                }
+            }
+
+            return;
+        }
+
+        // For minutes
+        if (letter.type === 'minute') {
+            if (letter.rel.includes(minutes)) {
+                letter.class.push('active');
+            }
+        }
     });
+
+    renderClockLetters(letters);
 };
 
-renderLetters(clockLetters());
+init();
